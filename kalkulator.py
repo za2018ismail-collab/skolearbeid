45658a3c80dfb9dce91fb2989c2ff7243f746c4a
za2018ismail-collab <z.a2018.ismail@gmail.com>
2026-09-03 11:50:37 +0200

    Add files via upload

---
+++ kalkulator.py
@@ -0,0 +1,96 @@
+# Steg 1: Python   
+print("Hei")
+print(5)
+print("Hei jeg heter Zana  og jeg er 42 år gammel, og jeg bor i Oslo")
+# skriv ut tre forskjelloge tall
+print(10)
+print(25)
+print(42)
+
+#forskell på tekst og tall
+print("5")  # Dette er tekst ( står i anførselstegn)
+print("5")  #Dette er et tall (uten anførselstegn)
+
+# Steg 2 Variabler gir verdier navn
+
+navn = "Sara"
+alder = 32
+print(navn)
+print(alder)
+
+#Steg 2, oppgave 1: Variabler gir verdier navn
+navn = "Zana"
+alder = 42
+bosted = "Oslo"
+
+print(navn)
+print(alder)
+print(bosted)
+
+#Steg 3 Vi kan regne med variabler
+pris = 25
+antall = 3 
+total = pris * antall
+print(total)
+
+print("skriv inn et tall")
+tall1 = int(input())
+print("skriv inn et tall til")
+tall2 =int(input())
+sum = tall1 + tall2
+print("Summen av de to tallene er", sum)
+
+#oppgave 2- fra faste verdier til beregning
+tall= 10
+tall= 5
+
+# Regn ut sammen og lagre svaret i en ny variabeil
+Summen = tall + tall2#  småoppgave for stage 1
+
+# Regn Ut produktet på samme måte
+produktet = tall * tall2
+
+# skriv ut resultatene
+print("summen er:summen")
+print("produktet er:",produktet)
+
+# Steg 4 Input fra bruknavn 
+
+navn = input("Hva heter du?")
+print("Hei,", navn)
+
+# oppgave 3 
+navn= input("Hva heter du? ")
+bosted= input("Hvor bor du? ")
+print("Hei,", navn, "du bor i", bosted)
+
+# steg 5: Input av tall
+alder = int(input("hvor gammel er du ?"))
+print("Du er", alder, "år gammel")
+
+# oppgave 4 alder fram i ti
+
+alder = int(input) ("hvor gammel er du?")
+print ("om 10 år er du", alder + 10)
+print("om 20 år er du", alder + 20)
+ 
+aar_fram = int(input("hvor mange år er du?"))
+print("om",aar_fram,"ær er du", alder + aar_fram)
+# Nivæ1
+tall1 = int(input("skriv inn det første heltallet: "))
+tall2 = int(input("skriv inn det andre heltallet: "))
+summen = tall1 + tall2
+print = ("summen av de to tallene er:",summen)
+
+# Mivå 2
+
+tall1 =  int(input("skriv inn det  første heltallet:"))
+tall2 = int (input("skriv inn det andre heltallet:"))
+summen = tall1 + tall2
+differanse = tall1 - tall2
+produkt = tall1 * tall2
+gjennomsnit = tall1 + tall2 /2
+print("summen er:, summen")
+print("differanse er:, differans")
+print("produkt er:, produkt")
+print("gjennomsnit er :, gjennomsnit")
